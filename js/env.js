65da806e66c3b995bc98c46d6765128d6c7e75d4
mrxsierra <sunilsharma9@outlook.com>
2025-04-01 20:24:59 +0530

    resolving html path for local and deploy

--- js/env.js
+++ js/env.js
@@ -1,10 +1,9 @@
 // Determine base path dynamically
 let isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
-console.log(isLocal);
 let base = isLocal ? "./" : "/test-site/";
 
 export let basepath = base;
-export let html_assets_path = base.endsWith("/") ? base + "html_assets/" : base + "/html_assets/";
+export let html_assets_path = base + "html_assets/";
 
 console.log(basepath);
 console.log(html_assets_path);
